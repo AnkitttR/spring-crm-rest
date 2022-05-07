6bb6f3f353ae98388e1908234e4d1df8b7519d15
Ankit <ankitthakur1998@gmail.com>
2022-05-07 11:20:28 +0530

    / Add an exception handler for CustomerNotFoundException is modified

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestExceptionHandler.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.luv2code.springdemo.rest;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,15 +10,18 @@ public class CustomerRestExceptionHandler {
 
 	// Add an exception handler for CustomerNotFoundException
 	@ExceptionHandler
-	public ResponseEntity<CustomerErrorResponse> handleException(CustomerNotFoundException exc) {
+	public ResponseEntity<CustomerErrorResponse> handleException(Exception exc) {
 		
 		// create CustomerErrorResponse
+		
+		CustomerErrorResponse error = new CustomerErrorResponse(HttpStatus.NOT_FOUND.value(),exc.getMessage(),
+																	System.currentTimeMillis());
 		
 		// return ResponseEntity
 		
 		
 		
-		return null;
+		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
 		
 		
 	}
